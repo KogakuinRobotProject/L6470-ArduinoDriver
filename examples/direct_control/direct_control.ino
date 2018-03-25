a1a29ba43b9ac35d92961e7c60da7c11f684c80e
TakashiKusachi <b514042@ns.kogakuin.ac.jp>
2018-03-26 03:41:45 +0900

    direct_control.ino更新
    簡単に作りました。

--- examples/direct_control/direct_control.ino
+++ examples/direct_control/direct_control.ino
@@ -5,15 +5,19 @@ L6470 StepperMotor(11);
 
 void setup() {
   // put your setup code here, to run once:
-  while(L6470_OK != StepperMotor.begin(){
+  while(L6470_OK != StepperMotor.begin()){
   }
   
-  StepperMotor.Acc.Set(100);
-  StepperMotor.Dec.Set(100);
-  StepperMotor.MaxSpeed.Set(100);
+  StepperMotor.Acc = 0x08A;       //IC default Parameter 0x08A
+  StepperMotor.Dec = 0x08A;       //IC default Parameter 0x08A
+  StepperMotor.MaxSpeed = 0x041;  //IC default Parameter 0x041
+  StepperMotor.MinSpeed = 0x000;  //IC default Parameter 0x000
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  StepperMotor.Move(0,100);
+  delay(1000);
+  StepperMotor.Move(1,100);
+  delay(1000);
 }
